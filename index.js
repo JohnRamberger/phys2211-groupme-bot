548f23fd92638cfaf63dd0d7f2e46e18fd9f139c
JohnRamberger <rambergerjohn@gmail.com>
2021-09-29 11:10:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http, director, bot, router, server, port;
 http = require('http');
 director = require('director');
 bot = require('./bot.js');
+const fs = require('fs');
 
 router = new director.http.Router({
     '/': {
@@ -29,4 +30,14 @@ server.listen(port);
 function ping() {
     this.res.writeHead(200);
     this.res.end("Hey! the chatbot is currently running :)");
+}
+
+var announcementsRaw = fs.readFileSync('announcements.json');
+var ann = JSON.parse(announcementsRaw);
+for(var a of ann){
+    console.log(a);
+}
+
+function sendAnnouncement(){
+
 }
