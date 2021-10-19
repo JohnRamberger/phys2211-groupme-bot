343bdbb5674c88e1c279c7821c1d3d3ed111d29d
JohnRamberger <rambergerjohn@gmail.com>
2021-10-19 00:23:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
-var HTTPS = require('https');
-const fs = require('fs');
-const db = require('./db.js');
+var HTTPS = require("https");
+const fs = require("fs");
+const db = require("./db.js");
+//quotes
+const Quote = require("inspirational-quotes");
 
 var botID = process.env.BOT_ID;
 
@@ -14,27 +16,41 @@ async function respond() {
     var text = String(request.text).toLowerCase().trim();
 
     //load the json file
-    var rawdata = fs.readFileSync('input.json');
+    var rawdata = fs.readFileSync("input.json");
     var input = JSON.parse(rawdata);
 
-    var match = false;
-    var i = 0;
-    while (i < input.length) {
-        if (input[i].keywords.indexOf(text) >= 0) {
+    switch (input[i].keywords) {
+        case "$quote":
+        case "$motivation":
             this.res.writeHead(200);
             //delay sending of message to prevent predicting the future
+            let motivation = Quote.getQuote();
             setTimeout(() => {
-                postMessage(input[i].response);
+                postMessage(`${motivation.text} —${motivation.author}`);
             }, 1000);
             this.res.end();
+            break;
+        default:
+            var match = false;
+            var i = 0;
+            while (i < input.length) {
+                if (input[i].keywords.indexOf(text) >= 0) {
+                    this.res.writeHead(200);
+                    //delay sending of message to prevent predicting the future
+                    setTimeout(() => {
+                        postMessage(input[i].response);
+                    }, 1000);
+                    this.res.end();
 
-            //update statistics
-            /*await db.insert_command(request.user_id, text, new Date());
-            let count = await db.command_count(text) + 1;
-            console.log(`${text} command used ${count} times`);*/
-            return;
-        }
-        i++;
+                    //update statistics
+                    /*await db.insert_command(request.user_id, text, new Date());
+                    let count = await db.command_count(text) + 1;
+                    console.log(`${text} command used ${count} times`);*/
+                    return;
+                }
+                i++;
+            }
+            break;
     }
 
     console.log("message sent from " + request.name);
@@ -48,34 +64,33 @@ function postMessage(_message) {
     botResponse = _message;
 
     options = {
-        hostname: 'api.groupme.com',
-        path: '/v3/bots/post',
-        method: 'POST'
+        hostname: "api.groupme.com",
+        path: "/v3/bots/post",
+        method: "POST",
     };
 
     body = {
-        "bot_id": botID,
-        "text": botResponse
+        bot_id: botID,
+        text: botResponse,
     };
 
-    console.log('sending ' + botResponse + ' to ' + botID);
+    console.log("sending " + botResponse + " to " + botID);
 
-    botReq = HTTPS.request(options, function(res) {
+    botReq = HTTPS.request(options, function (res) {
         if (res.statusCode == 202) {
             //neat
         } else {
-            console.log('rejecting bad status code ' + res.statusCode);
+            console.log("rejecting bad status code " + res.statusCode);
         }
     });
 
-    botReq.on('error', function(err) {
-        console.log('error posting message ' + JSON.stringify(err));
+    botReq.on("error", function (err) {
+        console.log("error posting message " + JSON.stringify(err));
     });
-    botReq.on('timeout', function(err) {
-        console.log('timeout posting message ' + JSON.stringify(err));
+    botReq.on("timeout", function (err) {
+        console.log("timeout posting message " + JSON.stringify(err));
     });
     botReq.end(JSON.stringify(body));
 }
-
 
 exports.respond = respond;
